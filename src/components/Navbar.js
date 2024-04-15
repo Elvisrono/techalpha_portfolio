fb4cb2c27f108af8a14e5a38f39983547762faee
ELVIS RONO <Elvis>
2024-04-15 14:42:40 +0300

    add navbar folder

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-scroll'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <div className='desktopMenu'>
+            <Link className='menulist'>Home</Link>
+            <Link className='menulist'>About</Link>
+            <Link className='menulist'>Portfolio</Link>
+            <Link className='menulist'>Clients</Link>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
