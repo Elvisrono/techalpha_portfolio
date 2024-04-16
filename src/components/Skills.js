01280c6634fc0682f9ae4e753064ec496fe15566
ELVIS RONO <Elvis>
2024-04-17 01:15:16 +0300

    finish skills folder

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,7 +3,29 @@ import React from 'react'
 const Skills = () => {
   return (
    <section id='skills'>
-     
+     <span className='skilltitle'>What I do</span><br />
+     <span className='skilldesc'>I am a skilled and passionate web designer with experience in web development</span>
+     <div className='skillbars'>
+        <div className='skillbar'>
+            <div className='skillbartext'>
+                <h2>UI/UX Design</h2>
+                <p>I design an interactive UI </p>
+            </div>
+        </div>
+        <div className='skillbar'>
+            <div className='skillbartext'>
+                <h2>web designer</h2>
+                <p>I design an interactive UI </p>
+            </div>
+        </div>
+        <div className='skillbar'>
+            <div className='skillbartext'>
+                <h2>UI/UX Design</h2>
+                <p>I design an interactive UI </p>
+            </div>
+        </div>
+
+     </div>
    </section>
   )
 }
